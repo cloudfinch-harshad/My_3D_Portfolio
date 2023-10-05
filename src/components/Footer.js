1dfa16799f3cc0ad46c776cbfc5275f57bf52d45
HARSHAD <87168540+HarshadHindlekar@users.noreply.github.com>
2023-10-05 10:59:33 +0530

    add links. (#2)

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,8 @@ import { Container, Row, Col } from "react-bootstrap";
 import { MailchimpForm } from "./MailchimpForm";
 import logo from "../assets/img/logo.svg";
 import navIcon1 from "../assets/img/nav-icon1.svg";
-import navIcon2 from "../assets/img/nav-icon2.svg";
-import navIcon3 from "../assets/img/nav-icon3.svg";
+import navIcon3 from "../assets/img/nav-icon2.svg";
+import navIcon2 from "../assets/img/nav-icon3.svg";
 
 export const Footer = () => {
   return (
@@ -16,11 +16,11 @@ export const Footer = () => {
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              <a href="#"><img src={navIcon1} alt="Icon" /></a>
-              <a href="#"><img src={navIcon2} alt="Icon" /></a>
+              <a href="https://www.linkedin.com/in/harshad-hindlekar-b94a07236/"><img src={navIcon1} alt="linkdin" /></a>
+              <a href="https://www.instagram.com/h_a_r_s_h_a_d_2_4/"><img src={navIcon2} alt="instagram" /></a>
               <a href="#"><img src={navIcon3} alt="Icon" /></a>
             </div>
-            <p>Copyright 2022. All Rights Reserved</p>
+            <p>Copyright 2023. All Rights Reserved</p>
           </Col>
         </Row>
       </Container>
