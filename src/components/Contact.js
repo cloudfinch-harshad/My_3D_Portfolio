99c3417fa67cfc23629228bf4ac3973128639790
HARSHAD <87168540+HarshadHindlekar@users.noreply.github.com>
2023-10-05 20:17:28 +0530

    Orbiting earth (#4)
    * add links.
    
    * Inserting logo
    
    * view
    
    * Add Orbiting earth in 3D

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,8 @@
 import { useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import contactImg from "../assets/img/contact-img.svg";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
+import EarthCanvas from "./Earth";
 
 export const Contact = () => {
   const formInitialDetails = {
@@ -36,7 +36,7 @@ export const Contact = () => {
     setButtonText("Send");
     let result = await response.json();
     setFormDetails(formInitialDetails);
-    if (result.code == 200) {
+    if (result.code === 200) {
       setStatus({ succes: true, message: 'Message sent successfully'});
     } else {
       setStatus({ succes: false, message: 'Something went wrong, please try again later.'});
@@ -48,10 +48,8 @@ export const Contact = () => {
       <Container>
         <Row className="align-items-center">
           <Col size={12} md={6}>
-            <TrackVisibility>
-              {({ isVisible }) =>
-                <img className={isVisible ? "animate__animated animate__zoomIn" : ""} src={contactImg} alt="Contact Us"/>
-              }
+            <TrackVisibility className ="earth-container">
+              <EarthCanvas />
             </TrackVisibility>
           </Col>
           <Col size={12} md={6}>
