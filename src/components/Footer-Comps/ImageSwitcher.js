bd15c9c87be0b065bd6790d86c1dd41a73bf5fb3
HARSHAD <87168540+HarshadHindlekar@users.noreply.github.com>
2023-10-09 12:03:16 +0530

    raise a fix (#8)

--- src/components/Footer-Comps/ImageSwitcher.js
+++ src/components/Footer-Comps/ImageSwitcher.js
@@ -1,8 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import "../../css/ImageSwitcher.css";
 import imagePath2 from "../../assets/img/me-after.svg";
-import imagepath1 from "../../assets/img/me-before.svg";
-
 
 const ImageSwitcher = () => {
   const [isEndOfPage, setIsEndOfPage] = useState(false);
@@ -12,9 +10,7 @@ const ImageSwitcher = () => {
       const isAtEndOfPage = window.innerHeight + window.scrollY >= document.body.offsetHeight;
       setIsEndOfPage(isAtEndOfPage);
     };
-
     window.addEventListener('scroll', handleScroll);
-
     return () => {
       window.removeEventListener('scroll', handleScroll);
     };
@@ -25,9 +21,6 @@ const ImageSwitcher = () => {
       <div className={`image-transition ${isEndOfPage ? 'fade-in' : 'fade-out'}`}>
         <img src={imagePath2} alt='my-pic' className='my-image' />
         <h1> Thank for visit !</h1>
-      </div>
-      <div className={`image-transition ${isEndOfPage ? 'fade-out' : 'fade-in'}`}>
-        <img src={imagepath1} alt='my-pic-old' className='my-image-old' />
       </div>
     </div>
   );
